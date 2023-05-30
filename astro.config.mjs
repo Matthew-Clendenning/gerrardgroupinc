bab5e092cf4788c221ff8ddce81a317ecb5c1363
Matthew Clendenning <matthewclendenning@outlook.com>
2023-05-29 23:51:17 -0500

    config commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerrardgroupinc.netlify.app',
-  integrations: [react(), partytown(), sitemap()]
+  integrations: [react(), sitemap()]
 });
