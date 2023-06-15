e365998268ec7d889c553d5a8d118753f37e4be1
Matthew Clendenning <matthewclendenning@outlook.com>
2023-06-15 17:36:47 -0500

    images commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerrardgroupinc.netlify.app',
   integrations: [react(), sitemap({
-    filter: (page) => 
-      page !== "https://gerrardgroupinc.netlify.app/PacificHP/" &&
-      page !== "https://gerrardgroupinc.netlify.app/timkenBearings/" &&
-      page !== "https://gerrardgroupinc.netlify.app/timkenDetails/",
-  })]
+    filter: page => page !== "https://gerrardgroupinc.netlify.app/PacificHP/" && page !== "https://gerrardgroupinc.netlify.app/timkenBearings/" && page !== "https://gerrardgroupinc.netlify.app/timkenDetails/"
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
