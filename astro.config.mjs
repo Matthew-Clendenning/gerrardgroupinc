8cc77046d07598e9711e93ee99305d0fb1cd5e48
Matthew Clendenning <matthewclendenning@outlook.com>
2023-06-15 19:56:10 -0500

    folder structure commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerrardgroupinc.netlify.app',
+  experimential: {
+    assets: true,
+  },
   integrations: [react(), sitemap({
     filter: page => page !== "https://gerrardgroupinc.netlify.app/PacificHP/" && page !== "https://gerrardgroupinc.netlify.app/timkenBearings/" && page !== "https://gerrardgroupinc.netlify.app/timkenDetails/"
   }), image({
