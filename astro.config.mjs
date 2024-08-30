af045296ae82257b911aa71e083548865813ac9d
Matthew Clendenning <124844665+Matthew-Clendenning@users.noreply.github.com>
2024-08-29 21:36:06 -0500

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   integrations: [react(), sitemap({
     priority: 0.9,
-    lastmod: new Date('2024-08-27'),
+    lastmod: new Date('2024-08-29'),
     i18n: {
       defaultLocale: 'en',
       locales: {
--- indexnow-notify.js
+++ indexnow-notify.js
@@ -6,4 +6,4 @@ async function updatePage(urls) {
 }
 
 // Example usage
-updatePage(['https://gerrardgroupinc.com/liquidations']);
+updatePage(['https://gerrardgroupinc.com/atlas-copco-air-compressor-1550cd7/', 'https://gerrardgroupinc.com/atlas-copco-air-compressor-1600/']);
