f5d02e66f1f0a7ccb689e1d68bc565245c585ae7
Matthew Clendenning <124844665+Matthew-Clendenning@users.noreply.github.com>
2026-03-16 13:19:21 -0500

    Removed files

--- indexnow-notify.js
+++
@@ -1,8 +0,0 @@
-import { notifyIndexNow } from './indexnow.js';
-
-async function updatePage(urls) {
-    // Notify IndexNow about the updated pages
-    await notifyIndexNow(urls);
-}
-
-updatePage(['https://gerrardgroupinc.com/atlas-copco-air-compressor-1550-cd/', 'https://gerrardgroupinc.com/atlas-copco-air-compressor-1550-cd7/']);
